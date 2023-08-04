e933fbc023f919ebdebcc04e505b875318a0694c
Milan Lee <100390926+doremilan@users.noreply.github.com>
2023-08-04 11:34:39 +0900

    fix: 스타일 적용

--- week-1/src/components/SideBarHeader/index.js
+++ week-1/src/components/SideBarHeader/index.js
@@ -1,5 +1,7 @@
+import './index.css';
+
 function SideBarHeader() {
-  return <div className="SideBarHeader">SideBarHeader</div>;
+  return <div className="SideBarHeader">메모장</div>;
 }
 
 export default SideBarHeader;
